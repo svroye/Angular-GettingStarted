8975c7027406156059790541929d91f5b6b831c8
Stevenvr <vanroyesteven@gmail.com>
2018-12-03 06:29:52 +0100

    first exercises for product list

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  // declare which of our components belong to this module
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
+  // external modules that we want to have available to all of the components
+  // that belong to this module
   imports: [
     BrowserModule
   ],
+  // start up component of the application. This component should contain the
+  // selector we use in the index.html
   bootstrap: [AppComponent]
 })
 export class AppModule { }
